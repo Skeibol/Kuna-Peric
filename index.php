d3da8d3a62175a5bf4b2074069cd5725c2f05fe7
mrzljak_ines <mrzljakines@gmail.com>
2023-09-26 23:59:47 +0200

    za uplakanu bebicu

--- index.php
+++ index.php
@@ -5,109 +5,166 @@ include "header.php";
 
 
 
-<div class="hero--container--mobile">
-    <img class="hero--image--mobile" src="images/peric.png" alt="">
-    <h1 class="hero--cta--mobile"><span class="hero--cta--zdravlje--mobile">Zdravlje</span> na prvom<br> mjestu</h1>
-    <button class='button--mobile'>Dogovori termin</button>
+    <div class="hero--container">
+        <div class="hero--image--bg">
+            <div class="hero--cta d-flex">
+                <div class="hero--dr--img col-sm-12 col-md-4 col-lg-6">
+                    <!-- Your image content here -->
+                </div>
+                <div
+                    class="hero--cta--text col-sm-12 col-md-8 col-lg-6 d-flex flex-column gap-lg-5 justify-content-center justify-content-sm-start">
+                    <h1 class="hero--cta--slogan d-none d-sm-block"><span class="hero--cta--zdravlje">Zdravlje</span> na prvom mjestu
+                    </h1>
+                    <p class="d-none d-sm-block">Medicinski centar Kuna&Perić - pouzdana destinacija za brigu o Vašoj dobrobiti</p>
+                    <div class="hero--button--container d-flex justify-content-center justify-content-sm-start">
+                        <button class='button--bg d-none d-sm-block'>Više o nama</button>
+                        <button class='button--nobg d-none d-sm-block'>Dogovori termin</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="hero--cta--slogan d-sm-none"><span class="hero--cta--zdravlje">Zdravlje</span> na prvom mjestu
+                    </h1>
+                    <div class="hero--button--container d-flex justify-content-center justify-content-sm-start">
+                        <button class='button--nobg d-sm-none'>Dogovori termin</button>
+                    </div>
+        <!-- <img class="hero--image--mobile" src="images/peric.png" alt=""> -->
+    
+    </div>
+    
+    <div class="container--default">
+        <div class="services--home">
+            <div
+                class="services--home--box col-sm-12 col-md-4 col-lg-4 d-flex flex-column align-items-center justify-content-space-around">
+                <img src="images/usluga.png" alt="">
+                <h4>Blefaroplastika</h4>
+                <p>Za odmoran izgled očiju koje zrače pričom o mladosti i vitalnosti</p>
+            </div>
+            <div
+                class="services--home--box col-sm-12 col-md-4 col-lg-4 d-flex flex-column align-items-center justify-content-space-around">
+                <img src="images/usluga.png" alt="">
+                <h4>Okuloplastična kirurgija</h4>
+                <p>Podarite očima novi sjaj i osjetite dodatno samopouzdanje</p>
+            </div>
+            <div
+                class="services--home--box col-sm-12 col-md-4 col-lg-4 d-flex flex-column align-items-center justify-content-space-around">
+                <img src="images/usluga.png" alt="">
+                <h4>Botox</h4>
+                <p>Osvježite prirodnu ljepotu i povratite mladenački sjaj</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<div class="container--default">
+    <div class="abt--us--home d-flex">
+        <div class="abt--us--img order-lg-1 order-2 col-md-6 col-lg-4 d-sm-none d-lg-block"><img class="justify-self-center" src="images/doktorik.png" alt="">
+        </div>
+        <div class="abt--us--text order-lg-2 col-lg-8">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <h3>O NAMA</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et expedita soluta fugit voluptate ut mollitia
+                reprehenderit natus distinctio quaerat repellat inventore excepturi ipsa sequi facere accusamus
+                praesentium,
+                dolorem, quia hic! Dolore eius soluta dolores, explicabo quia in possimus rerum mollitia libero, cum
+                similique.
+                Voluptas itaque placeat consequatur voluptate minus ad?</p>
+            <div class="abt--us--md">
+                <div class="abt--us--offer d-none d-md-block col-md-7 col-lg-12">
+                    <p>Medicinski centar Kuna & Perić predstavlja tim vrhunskih liječnika koji Vam svojim bogatim znanjem i
+                        iskustvom
+                        stoje na raspolaganju za područja:</p>
+                    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+                    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+                    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+                    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+                </div>
+                <div class="abt--us--img d-none d-md-block d-lg-none col-md-5 col-lg-12"><img class="justify-self-center" src="images/doktorik.png" alt="">
+                        </div>
+            </div>
+        </div>
+
+
+        <div class="abt--us--offer order-lg-3 order-3 d-sm-none">
+            <p>Medicinski centar Kuna & Perić predstavlja tim vrhunskih liječnika koji Vam svojim bogatim znanjem i
+                iskustvom
+                stoje na raspolaganju za područja:</p>
+            <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+            <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+            <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+            <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
+        </div>
+    </div>
 </div>
 
-<div class="d-flex flex-column align-items-center justify-content-center flex-md-row usluge--container--mobile mt-5">
-    <div class="usluge--fake--border"></div>
-    <div class="usluge--box--mobile d-flex flex-column align-items-center justify-content-center p-5 m-lg-5">
-        <img src="images/usluga.png" alt="">
-        <h2>Blefrastika</h2>
-        <p>Dodite kod nas na blefrastiku jajaja dat cemo vam je</p>
-    </div>
-    <div class="usluge--fake--border"></div>
-    <div class="usluge--box--mobile d-flex flex-column align-items-center justify-content-center p-5 m-lg-5">
-        <img src="images/usluga.png" alt="">
-        <h2>Blefrastika</h2>
-        <p>Dodite kod nas na blefrastiku jajaja dat cemo vam je</p>
-    </div>
-    <div class="usluge--fake--border"></div>
-    <div class="usluge--box--mobile d-flex flex-column align-items-center justify-content-center p-5 m-lg-5">
-        <img src="images/usluga.png" alt="">
-        <h2>Blefrastika</h2>
-        <p>Dodite kod nas na blefrastiku jajaja dat cemo vam je</p>
-    </div>
-</div>
-<div class="mobile--default--margin d-flex flex-column">
-    <h2>Lorem ipsum dolor sit amet.</h2>
-    <h3>O NAMA</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et expedita soluta fugit voluptate ut mollitia
-        reprehenderit natus distinctio quaerat repellat inventore excepturi ipsa sequi facere accusamus praesentium,
-        dolorem, quia hic! Dolore eius soluta dolores, explicabo quia in possimus rerum mollitia libero, cum similique.
-        Voluptas itaque placeat consequatur voluptate minus ad?</p>
-    <img class="justify-self-center" src="images/doktorik.png" alt="">
-
-    <p>Medicinski centar Kuna & Perić predstavlja tim vrhunskih liječnika koji Vam svojim bogatim znanjem i iskustvom
-        stoje na raspolaganju za područja:</p>
-    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
-    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
-    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
-    <p><img style="height:25px;width:25px;" src="images/plus.png" alt="">Miševi</p>
-</div>
 
 
-
-<div class="stats--box--container--mobile d-flex flex-column align-items-center justify-content-center">
-    <div class="stats--box--mobile">
-        <h3 class='span--highlight'>3200</h3>
-        <p>Zadovoljnih korisnika</p>
-    </div>
-    <div class="stats--box--mobile">
-        <h3 class='span--highlight'>3200</h3>
-        <p>Zadovoljnih korisnika</p>
-    </div>
-    <div class="stats--box--mobile">
-        <h3 class='span--highlight'>3200</h3>
-        <p>Zadovoljnih korisnika</p>
-    </div>
-    <div class="stats--box--mobile">
-        <h3 class='span--highlight'>3200</h3>
-        <p>Zadovoljnih korisnika</p>
+<div class="stats--box--container d-flex flex-column flex-md-row align-items-center">
+    <div class="stats--wrapper">
+        <div class="stats--box col-md-3">
+            <h3 class='span--highlight'>3200</h3>
+            <p>Zadovoljnih korisnika</p>
+        </div>
+        <div class="stats--box col-md-3">
+            <h3 class='span--highlight'>3200</h3>
+            <p>Zadovoljnih korisnika</p>
+        </div>
+        <div class="stats--box col-md-3">
+            <h3 class='span--highlight'>3200</h3>
+            <p>Zadovoljnih korisnika</p>
+        </div>
+        <div class="stats--box col-md-3">
+            <h3 class='span--highlight'>3200</h3>
+            <p>Zadovoljnih korisnika</p>
+        </div>
     </div>
 </div>
 
-<div class="mobile--default--margin">
+<div class="container--default">
     <h2>Zašto izabrati nas?</h2>
     <p>Medicinski centar Kuna&Perić se ističe po vrhunskoj kvaliteti skrbi, stručnom osoblju i modernoj tehnologiji.
         Naša posvećenost Vašem dobrostanju osigurava Vam najbolju njegu prilagođenu Vašim specifičnim potrebama. Iza nas
         je dugogodišnji rad i velik broj zadovoljnih pacijenata, koji svjedoče o našoj posvećenosti. U medicinskom
         centru Kuna&Perić živimo svoje pozive svakodnevno pomažući onima koji nas trebaju. Obratite nam se za sve Vaše
         medicinske potrebe i doživite vrhunsku razinu skrbi koju zaslužujete.</p>
-    <button class="button--mobile--inverse">Dogovori termin</button>
+    <button class="button--bg">Dogovori termin</button>
 </div>
-
-<div class="officeimage--background--mobile">
-    <div class="mobile--default--margin ">
-        <div class="officeimage--container--mobile">
-
-            <div class="officeimage officeimage--left">
-                <img src="images/ured1.png" alt="" srcset="">
-            </div>
-            <div class="officeimage officeimage--right">
-                <img src="images/ured2.png" alt="" srcset="">
-                <img src="images/ured3.png" alt="" srcset="">
-
+<div class="container--default">
+    
+    <div class="officeimage--background--mobile">
+        <div class="">
+            <div class="officeimage--container--mobile">
+    
+                <div class="officeimage officeimage--left">
+                    <img src="images/ured1.png" alt="" srcset="">
+                </div>
+                <div class="officeimage officeimage--right">
+                    <img src="images/ured2.png" alt="" srcset="">
+                    <img src="images/ured3.png" alt="" srcset="">
+    
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="mobile--default--margin">
+<div class="container--default">
     <h2>Što kažu naši klijenti</h2>
     <p>Vaša dobrobit je naš najveći uspjeh</p>
     <hr>
     KOMENTARI PACJENTATA POSLOZENI LIJEPO
     <hr>
 </div>
-<div class="mobile--default--margin">
 
+
+<div class="container--default">
     <div class="accordion accordion-flush" id="accordionFlushExample">
-        <div class="container">
+        <div class="container--default">
             <div class="row">
-                <div class="col p-0 mb-4">
+                <div class="col-sm-12 col-md-12 col-lg-6 p-0 mb-3">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingOne">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -127,7 +184,7 @@ include "header.php";
                 </div>
             </div>
             <div class="row">
-                <div class="col p-0 mb-4">
+                <div class="col-sm-12 col-md-12 col-lg-6 p-0 mb-3">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -148,8 +205,9 @@ include "header.php";
                     </div>
                 </div>
             </div>
+
             <div class="row">
-                <div class="col p-0 mb-4">
+                <div class="col-sm-12 col-md-12 col-lg-6 p-0 mb-3">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -173,33 +231,31 @@ include "header.php";
                 </div>
             </div>
         </div>
-
-
-
     </div>
 </div>
 
-<div class="mobile--default--margin">
+<div class="container--default">
     <h2>Rezervirajte svoj termin</h2>
     <p>Trendovi ljepote se mijenjaju, ali zdravlje nikada ne izlazi iz mode. U Medicinskom centru Kuna&Perić brinemo o
         Vašem zdravlju i samopouzdanju. Kontaktirajte nas kako bismo zajedno pronašli put prema Vašem osjećaju
         potpunosti i unutarnje harmonije.</p>
-    <form action="send">
-        <input class='form--box' type="text" placeholder="Ime i prezime"></input>
-        <input class='form--box' type="text" placeholder="Email"></input>
-        <input class='form--box' type="text" placeholder="Mobitel"></input>
-        <input class='form--box' type="text" placeholder="Email"></input>
-        <select class="form-select dropdown--menu" aria-label="Default select example">
-            <option selected>Usluga</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-        </select>
-        <textarea class='form--textarea' type="text" placeholder="Poruka"></textarea>
-        <button class="button--mobile--inverse">Pošalji</button>
-    </form>
-</div>
-<img src="images/statua.png" alt="" srcset="">
+    <div class="contact--hero">
+        <form class="col-md-8" action="send" >
+            <input class='form--box' type="text" placeholder="Ime i prezime"></input>
+            <input class='form--box' type="email" placeholder="Email"></input>
+            <input class='form--box' type="phone" placeholder="Mobitel"></input>
+            <select class="form-select dropdown--menu" aria-label="Default select example">
+                <option selected>Usluga</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            <textarea class='form--textarea' type="text" placeholder="Poruka"></textarea>
+            <button class="button--bg">Pošalji</button>
+        </form>
+        
+        <img class="col-md-4" src="images/statua.png" alt="" srcset="">
+    </div></div>
 <img style='width: 100%' src="images/mapa.png" alt="" srcset="">
 
 
